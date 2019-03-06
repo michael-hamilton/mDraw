4a59c38b75dcd550acca93dab6c3730368620087
Michael Hamilton <michaelhamilton626@gmail.com>
2019-03-05 19:57:11 -0500

    Add temporary data-readout
    Adds a data readout div used to show data gathered from Apple Pencil

--- index.ts
+++ index.ts
@@ -17,5 +17,19 @@ const ctx = setupCanvas(canvas);
 
 const D = new Drawer(ctx);
 
+const updateReadout = e => {
+    const readout = document.getElementById('data-readout');
+    let text = [];
+        text.push("Ang: " + e.touches[0].altitudeAngle);
+        text.push("Az: " + e.touches[0].azimuthAngle);
+        text.push("X: " + e.touches[0].clientX);
+        text.push("rX: " + e.touches[0].radiusX);
+        text.push("Y: " + e.touches[0].pageY);
+        text.push("rY: " + e.touches[0].radiusY);
+        text.push("f: " + e.touches[0].force);
+     readout.innerHTML = text.join(' | ');
+};
+
+document.addEventListener('touchmove', e => updateReadout(e));
 document.addEventListener('touchmove', e => D.updateTouch(e.touches[0].clientX, e.touches[0].pageY, e.touches[0].force));
 document.addEventListener('touchend', e => D.resetTouch());
